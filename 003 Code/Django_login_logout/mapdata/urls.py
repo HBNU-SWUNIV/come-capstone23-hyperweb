a58c0e4afe3b2191980de4dd3972bad6e599cbd1
WSM <woodfire99@naver.com>
2023-04-04 23:28:54 +0900

    003Ver_Mapdata_Available

--- 003 Code/Django_login_logout/mapdata/urls.py
+++ 003 Code/Django_login_logout/mapdata/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/map', views.map, name='map'),
+    path('map/', views.map, name='map'),
 ]
--- 003 Code/Django_login_logout/mapdata/views.py
+++ 003 Code/Django_login_logout/mapdata/views.py
@@ -6,6 +6,6 @@ from .serializers import MapDataSerializer
 
 @api_view(['GET'])
 def map(request):
-    datas = Mapdata.objects.all()
+    datas = Mapdata.objects.all()[:100] # 100개로 제한
     serializer = MapDataSerializer(datas, many=True)
     return Response(serializer.data)
