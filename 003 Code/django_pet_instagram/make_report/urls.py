ab5a2a5287a55899f3c98dd006ab507967dff5c7
Koo <kooyh932@gmail.com>
2023-05-29 00:55:07 +0900

    화면캡쳐 업데이트

--- 003 Code/django_pet_instagram/make_report/urls.py
+++ 003 Code/django_pet_instagram/make_report/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-
+from django.urls import include,path
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('save_image/', views.save_image, name='save_image'),
+
 ]
--- 003 Code/django_pet_instagram/make_report/views.py
+++ 003 Code/django_pet_instagram/make_report/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from make_report.models import Nutrient
 from django.http import HttpResponse
+from django.conf import settings
+from django.core.files.base import ContentFile
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
+import base64
+import os
 
 
 def index(request):
@@ -17,3 +24,20 @@ def make_report(request):
         'nutrients': nutrients
     }
     return render(request, 'report.html', context)
+
+@csrf_exempt
+def save_image(request):
+    if request.method == 'POST':
+        format, imgstr = request.POST['image_base64'].split(';base64,') 
+        ext = format.split('/')[-1] 
+
+        data = ContentFile(base64.b64decode(imgstr))  
+        file_name = 'myimage.' + ext  
+        file_path = os.path.join('media/', file_name)
+
+        with open(file_path, 'wb') as f:
+            f.write(data.read())
+
+        return JsonResponse({'status': 'success'}, status=200)
+    else:
+        return JsonResponse({'status': 'fail'}, status=400)
